2a5c40b0be2db0902d77ff9016913350b155bd4e
AlexBroccoli <alex@qwoot.net>
2012-04-14 23:55:46 +0200

    refactor

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import subprocess
 from time import sleep
 
 class Plot:
-	def start(this, screen, args):
+	def start(self, screen, args):
 		screen_height, screen_width = screen.getmaxyx()
 
 		i = 0
@@ -14,6 +14,8 @@ class Plot:
 		scale_min = 0
 
 		values = []
+
+		pw = PlotWindow(screen)
 
 		while True:
 			i = i + 1
@@ -31,34 +33,48 @@ class Plot:
 			if value >= scale_max:
 				scale_max = value * 1.2
 
-			piece = scale_max / screen_height
-			scale_value = int(value / piece)
-
-			# invert because ncurses starts counting from top
-			# minus one because position cannot exactely be screen_height
-			scale_value = (screen_height - scale_value) - 1
-
-			values.append(scale_value)
+			values.append(value)
 
 			if len(values) >= screen_width:
 				values = values[1:]
 
-			# draw graph
-			screen.clear()
-
-			# minus 2 because screen is printable until width-1 and bottom right is unusable?
-			pos = screen_width-2
-			j = len(values)-1
-
-			while j >= 0:
-				screen.addstr(values[j], pos, '-')
-				j = j - 1
-				pos = pos - 1
+			pw.draw_graph(scale_min, scale_max, values)
 
 			screen.addstr(0, 0, 'running "{0}"'.format(args.command))
 			screen.addstr(1, 0, 'Got value {0}, max scale {1}'.format(value, scale_max))
 			screen.refresh()
 			sleep(1)
+
+class PlotWindow:
+	screen_height = 0
+	screen_width = 0
+	screen = False
+
+	def __init__(self, screen):
+		self.screen_height, self.screen_width = screen.getmaxyx()
+		self.screen = screen
+
+	def draw_graph(self, scale_min, scale_max, values):
+		self.screen.clear()
+
+		# minus 2 because screen is printable until width-1 and bottom right is unusable?
+		pos = self.screen_width-2
+		j = len(values)-1
+
+		piece = scale_max / self.screen_height
+
+		while j >= 0:
+			value = values[j]
+			scale_value = int(value / piece)
+
+			# invert because ncurses starts counting from top
+			# minus one because position cannot exactely be screen_height
+			scale_value = (self.screen_height - scale_value) - 1
+
+			self.screen.addstr(scale_value, pos, '-')
+			j = j - 1
+			pos = pos - 1
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Plot values in a graph')
